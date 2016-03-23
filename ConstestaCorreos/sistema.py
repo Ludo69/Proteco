99f48faceae0db5397ca1db75fe26307ea6a5290
Ludo69 <somos305@gmail.com>
2016-03-23 10:44:30 -0600

    Uno

---
+++ ConstestaCorreos/sistema.py
@@ -0,0 +1,31 @@
+ # -*- coding: utf-8 -*-
+import smtplib
+
+def enviar(x):
+	para=x
+
+	encabezado= 'To:'+para+'\n'+'From: Proteco <'+usuarioGmail+'> \n'+'MIME-Version: 1.0'+'\n'+'Content-type: text/html'+'\n'+'Subject: Prueba\n'
+	print(encabezado)
+
+	mensaje= encabezado+'\n<center><h1>Bienvenido a Battle Code!</h1><br><img src="http://s2.subirimagenes.com/otros/previo/thump_9546181ba.jpg"></center>'
+
+	try:
+		#hotmail -> "smtp.live.com, 587"
+		smtpServer=smtplib.SMTP("smtp.gmail.com",587)
+		smtpServer.ehlo()
+		smtpServer.starttls()
+		smtpServer.ehlo()
+		smtpServer.login(usuarioGmail,contraseña)
+		smtpServer.sendmail(usuarioGmail,para,mensaje)
+		print("Enviado!")
+	except Exception as e:
+		print("Error: No se puede conectar o enviar el correo")
+		print(e)
+		smtpServer.close()
+
+usuarioGmail='lara.proteco@gmail.com'
+contraseña='Battlecode_'
+
+listaCorreos = ['kurikosa1@hotmail.com', 'somos305@gmail.com']
+for x in listaCorreos:
+	enviar(x)
